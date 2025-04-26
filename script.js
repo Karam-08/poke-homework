ee5f172876c83a6fc15ab8fb30ede9f7c14fbabf
TheProgrammerofalltime <kma69362@gmail.com>
2025-04-25 18:20:59 -0700

    commit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let team = {}
+let cards = document.getElementById("pokemonCards")
 
 let pokeAPI = {"count":1302,"next":"https://pokeapi.co/api/v2/pokemon?offset=10&limit=10","previous":null,"results":[{"name":"bulbasaur","url":"https://pokeapi.co/api/v2/pokemon/1/"},{"name":"ivysaur","url":"https://pokeapi.co/api/v2/pokemon/2/"},{"name":"venusaur","url":"https://pokeapi.co/api/v2/pokemon/3/"},{"name":"charmander","url":"https://pokeapi.co/api/v2/pokemon/4/"},{"name":"charmeleon","url":"https://pokeapi.co/api/v2/pokemon/5/"},{"name":"charizard","url":"https://pokeapi.co/api/v2/pokemon/6/"},{"name":"squirtle","url":"https://pokeapi.co/api/v2/pokemon/7/"},{"name":"wartortle","url":"https://pokeapi.co/api/v2/pokemon/8/"},{"name":"blastoise","url":"https://pokeapi.co/api/v2/pokemon/9/"},{"name":"caterpie","url":"https://pokeapi.co/api/v2/pokemon/10/"}]}
 
@@ -14,6 +14,7 @@ let pokemon = fetch('https://pokeapi.co/api/v2/pokemon?limit=10')
     console.log('Pokemon Data: ', data)
     data.results.forEach(pokemon =>{
         console.log("Name:", pokemon.name)
+
         console.log("URL:", pokemon.url)
 
         fetch(pokemon.url)
@@ -24,10 +25,15 @@ let pokemon = fetch('https://pokeapi.co/api/v2/pokemon?limit=10')
             return response.json()
         })
         .then(info =>{
-            console.log(info.base_experience)
-            console.log(info.sprites.front_default)
-            console.log(info.types[0].type.name)
-            console.log(info.abilities[0].ability.name)
+            let card = document.createElement("div")
+            card.innerHTML = `
+            <h3>${pokemon.name}</h3>
+            <img src=${info.sprites.front_default}></img>
+            <p>Pokemon Type: ${info.types[0].type.name}</p>
+            <p>First Ability: ${info.abilities[0].ability.name}</p>
+            <p>Base EXP: ${info.base_experience}</p>
+            `
+            cards.appendChild(card)
         })
     })
     return data
