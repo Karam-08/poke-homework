dcd92c77f2e6b39b8d9f908210363713e8c63cf6
TheProgrammerofalltime <kma69362@gmail.com>
2025-04-28 21:40:04 -0700

    commit

--- script.js
+++ script.js
@@ -1,29 +1,12 @@
-let cards = document.getElementById("pokemonCards")
+let favoritePokemon = ['pikachu', 'charmander', 'charizard', 'greninja', 'lopunny', 'bulbasaur', 'gardevoir', 'squirtle', 'blastoise', 'vaporeon']
+const cards = document.getElementById("pokemonCards")
+const isIndex = document.getElementById("pokemonCards") // Used for toggling between functions
+const isPokedex = document.getElementById("searchBtn")
 
-let pokeAPI = {
-    "count": 1302,
-    "next": "https://pokeapi.co/api/v2/pokemon?offset=10&limit=10",
-    "previous": null,
-    "results":[
-        {"name":"bulbasaur","url":"https://pokeapi.co/api/v2/pokemon/1/"},{"name":"ivysaur","url":"https://pokeapi.co/api/v2/pokemon/2/"},{"name":"venusaur","url":"https://pokeapi.co/api/v2/pokemon/3/"},{"name":"charmander","url":"https://pokeapi.co/api/v2/pokemon/4/"},{"name":"charmeleon","url":"https://pokeapi.co/api/v2/pokemon/5/"},{"name":"charizard","url":"https://pokeapi.co/api/v2/pokemon/6/"},{"name":"squirtle","url":"https://pokeapi.co/api/v2/pokemon/7/"},{"name":"wartortle","url":"https://pokeapi.co/api/v2/pokemon/8/"},{"name":"blastoise","url":"https://pokeapi.co/api/v2/pokemon/9/"},{"name":"caterpie","url":"https://pokeapi.co/api/v2/pokemon/10/"}
-    ]
-}
-
-let pokemon = fetch('https://pokeapi.co/api/v2/pokemon?limit=10')
-.then(response =>{
-    if(!response.ok){
-        throw new Error('Network response was bad: ' + response.statusText)
-    }
-    return response.json()
-})
-
-.then(data => {
-    console.log('Pokemon Data: ', data)
-    data.results.forEach(pokemon =>{ // For each pokemon
-        console.log("Name:", pokemon.name)
-        console.log("URL:", pokemon.url)
-
-        fetch(pokemon.url) // Get pokemon stats
+// Index Page Logic
+if(isIndex){ // If the element pokemon Cards exists (only exists in the index page)
+    favoritePokemon.forEach(name =>{ // For each pokemon
+        fetch(`https://pokeapi.co/api/v2/pokemon${name}`) // Fetch their stats
         .then(response =>{
             if(!response.ok){
                 throw new Error('Network response was bad: ' + response.statusText)
@@ -31,20 +14,26 @@ let pokemon = fetch('https://pokeapi.co/api/v2/pokemon?limit=10')
             return response.json()
         })
         .then(info =>{ // Then create a card for each pokemon
-            let card = document.createElement("div")
+            let card = document.createElement("div") // Create a div to store card
+            // Add stuff to div
             card.innerHTML = `
-            <h3>${pokemon.name}</h3>
-            <img src=${info.sprites.front_default}></img>
+            <h3>${info.name}</h3>
+            <img src=${info.sprites.front_default} alt ="${info.name}"></img>
             <p>Pokemon Type: ${info.types[0].type.name}</p>
             <p>First Ability: ${info.abilities[0].ability.name}</p>
             <p>Base EXP: ${info.base_experience}</p>
             `
             cards.appendChild(card)
-            // Adds card to the container
+            // Adds div to the container
+        })
+        .catch(error =>{
+            console.error("There was a problem: ", error)
         })
     })
-    return data
-})
-.catch(error =>{
-    console.error("There was a problem: ", error)
-})
+}
+
+if(isPokedex){
+    
+}
+
+
